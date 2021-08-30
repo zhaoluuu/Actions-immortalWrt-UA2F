8bb215148486fb351516f0c33c1da1ac1d567b50
Linmo <30459943+DFlinmo@users.noreply.github.com>
2021-08-30 10:53:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/Zxilly/UA2F package/UA2F
+git clone https://github.com/DFlinmo/UA2F package/UA2F
 git clone https://github.com/Zxilly/JGSWeb package/JSGWeb
 git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
 git clone https://github.com/cnsilvan/UnblockNeteaseMusic package/UnblockNeteaseMusic
