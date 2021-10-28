d7a8da435f0183d2fded8045c1097ef4921d49ab
Linmo <30459943+DFlinmo@users.noreply.github.com>
2021-10-28 14:27:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
 
-git clone https://github.com/DFlinmo/UA2F package/UA2F
+git clone https://github.com/Zxilly/UA2F package/UA2F
 cd package/UA2F
 git checkout 20210531T234622
 cd ../..
